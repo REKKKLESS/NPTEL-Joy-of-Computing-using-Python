67e7f6688f6df7b8573a2f74c4bf645e5d60d160
REKKKLESS <107069125+REKKKLESS@users.noreply.github.com>
2022-10-17 08:37:43 +0530

    Create Week 4:Assignment_3.py

---
+++ Week 4:Assignment_3.py
@@ -0,0 +1,16 @@
+# Write a program to take an integer N as an input and display the pattern.
+
+# Input
+# 5
+
+
+N=int(input())
+for i in range(N):
+    for j in range(i+1):
+         print('* ',end="")
+    print()
+for i in range(N-1,0,-1):
+    for j in range(i):
+        print("* ",end="")
+    if j!=0:
+        print()
